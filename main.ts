b38b473fff92cb6126212f07473eb0e1cde394e8
agnotario <agnotario@gmail.com>
2024-09-27 11:48:21 +0200

    Actualizar pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+namespace serialCom {
+    // Define el pin que se utilizará para la comunicación serial
+    let txPin = DigitalPin.P0
+    let rxPin = DigitalPin.P1
 
+    /**
+     * Enviar un mensaje serial
+     * @param message Mensaje a enviar
+     */
+    //% block="enviar mensaje serial %message"
+    export function sendSerial(message: string): void {
+        serial.writeLine(message)
+    }
+}
